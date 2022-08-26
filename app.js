4e40b05b9d2098fadff23bfb4e634ce69f0cf02c
mihit <mihitsreejith.ps.2019.21@gmail.com>
2022-08-26 17:38:52 +0530

    changed path

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ async function predict_class() {
     .sub(tf.scalar(1.0))
     .expandDims(0);
 
-  const model = await tf.loadLayersModel("./model_4_edit.json");
-
+  //const model = await tf.loadLayersModel("./model_4_edit.json");
+  const model = await tf.loadLayersModel("https://newtonsmama.github.io/FishClassifier/model_4_edit.json");
   const pred = model.predict(step1).dataSync();
  
   console.log(pred);
